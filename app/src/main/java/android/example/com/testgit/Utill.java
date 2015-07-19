7209079029456956af2a10b6db9ff3ab4b93323a
almonick <almonick72@gmail.com>
2015-07-20 00:14:45 +0300

    init brnch colorls

---
+++ app/src/main/java/android/example/com/testgit/Utill.java
@@ -0,0 +1,12 @@
+package android.example.com.testgit;
+
+/**
+ * Created by Bender on 20/07/2015.
+ */
+public class Utill {
+    private boolean kuku = true;
+
+    public boolean getKuku(){
+        return kuku;
+    }
+}
